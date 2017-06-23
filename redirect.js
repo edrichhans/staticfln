eb5e8f8e569aab5f17441960500a71eb892ffb57
Edrich Hans Chua <echua@freelancer.com>
2017-06-23 14:52:48 +0800

    tinagalan

--- redirect.js
+++ redirect.js
@@ -1,15 +1,15 @@
 // alert('hi');
 $(document).ready(function() {
-    // chrome.runtime.getURL('hackathon/FREELANCER.html');
-    // setTimeout(function(){
-    //     window.location.href = 'https://www.freelancer.com';
-    // }, 5000);
+    chrome.runtime.getURL('hackathon/FREELANCER.html');
     setTimeout(function(){
-        console.log('ready!')
-        chrome.tabs.executeScript(null, {
-            code: 'var config = ' + JSON.stringify(getKeywords)
-        }, function() {
-            chrome.tabs.executeScript(null, {file: 'custom.js'});
-        });
-    }, 10000);
+        window.location.href = 'https://edrichhans.github.io/staticfln';
+    }, 5000);
+    // setTimeout(function(){
+    //     console.log('ready!')
+    //     chrome.tabs.executeScript(null, {
+    //         code: 'var config = ' + JSON.stringify(getKeywords)
+    //     }, function() {
+    //         chrome.tabs.executeScript(null, {file: 'custom.js'});
+    //     });
+    // }, 10000);
 });
